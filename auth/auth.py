acfcfd1e6a70457f4523d9184d620b55ff56db91
HY Devinton <femmyblaq@gmail.com>
2026-07-28 04:55:02 -0700

    testing

--- auth/auth.py
+++ auth/auth.py
@@ -63,7 +63,7 @@ def register():
         cursor.close()
 
         verification_link = (f"https://flask-auth-endpoint.onrender.com/api/auth/verify-email/{verification_token}")
-        send_verification_email(email, fullname, verification_link)
+        # send_verification_email(email, fullname, verification_link)
         return jsonify({
             "success": True,
             "message": "User registered successfully."
@@ -98,6 +98,7 @@ def verify_email(token):
     )
 
     user = cursor.fetchone()
+    print(f"Verifying user: {user}")
 
     if not user:
         cursor.close()
